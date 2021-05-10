520b599513fed23523897efa326d6ca706b8a774
Vladimir Tretyak <trvo@smart-dn.ru>
2021-05-10 08:40:57 +0300

    small edits

--- app/src/main/java/com/tavrida/energysales/ui/components/common/OrientationSelector.kt
+++ app/src/main/java/com/tavrida/energysales/ui/components/common/OrientationSelector.kt
@@ -3,7 +3,6 @@ package com.tavrida.energysales.ui.components.common
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import com.tavrida.energysales.ui.theme.CounterReaderTheme
-import com.tavrida.energysales.ui.view_models.CounterReadingViewModel
 
 @Composable
 fun OrientationSelector(content: @Composable () -> Unit) {
