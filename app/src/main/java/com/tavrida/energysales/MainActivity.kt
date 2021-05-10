cb39ac50c85e238c3da340eda91f39a20ba76f9b
Vladimir Tretyak <trvo@smart-dn.ru>
2021-05-10 17:55:42 +0300

    qr scanning

--- app/src/main/java/com/tavrida/energysales/MainActivity.kt
+++ app/src/main/java/com/tavrida/energysales/MainActivity.kt
@@ -1,27 +1,84 @@
 package com.tavrida.energysales
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.tavrida.energysales.data_access.DatabaseInstance
 import com.tavrida.energysales.data_access.models.DataContext
 import com.tavrida.energysales.ui.components.App
+import com.tavrida.energysales.ui.theme.CounterReaderTheme
 import com.tavrida.energysales.ui.view_models.CounterReadingViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+
+    private val permissions =
+        listOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    private val permissionsRequestCode = Random.nextInt(0, 10000)
+    private fun hasPermissions(context: Context) = permissions.all {
+        ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == permissionsRequestCode && hasPermissions(this)) {
+            ready()
+        }
+    }
+
+    private var permissionsGranted by mutableStateOf(false)
+
     private lateinit var viewModel: CounterReadingViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (hasPermissions(this)) {
+            ready()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, permissions.toTypedArray(), permissionsRequestCode
+            )
+        }
+
+        setContent {
+            CounterReaderTheme {
+                if (permissionsGranted) {
+                    App(viewModel)
+                } else {
+                    NoPermissions()
+                }
+            }
+        }
+    }
+
+    private fun ready() {
         viewModel = CounterReadingViewModel(
             DataContext(DatabaseInstance.get(filesDir))
         )
-        setContent {
-            App(viewModel)
-        }
+        permissionsGranted = true
 
         lifecycleScope.launchWhenCreated {
             withContext(Dispatchers.IO) {
@@ -46,5 +103,7 @@ class MainActivity : ComponentActivity() {
     }*/
 
     companion object {
+        @Composable
+        fun NoPermissions() = Text(text = "No permissions!")
     }
 }
