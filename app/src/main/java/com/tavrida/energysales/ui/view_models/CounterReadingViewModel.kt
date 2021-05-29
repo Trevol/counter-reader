d493434c913bca8db27c4a4591e449411e30f2c3
Vladimir Tretyak <trvo@smart-dn.ru>
2021-05-29 12:43:47 +0300

    sync stub

--- app/src/main/java/com/tavrida/energysales/ui/view_models/CounterReadingViewModel.kt
+++ app/src/main/java/com/tavrida/energysales/ui/view_models/CounterReadingViewModel.kt
@@ -194,23 +194,48 @@ private class CounterReadingsSynchronizer(dataContext: IDataContext) {
 
     private suspend fun syncInRealMode(allConsumers: List<Consumer>) {
         withContext(Dispatchers.IO) {
+            val unsynchronized = allConsumers.unsynchronizedReadings()
+            if (unsynchronized.isEmpty()) {
+                return@withContext
+            }
 
-            val items = listOf(
-                CounterReadingSyncItem(
-                    id = 1,
-                    user = AppSettings.user,
-                    counterId = 1,
-                    reading = 999.0,
-                    readingTime = LocalDateTime.now().toEpochMilli(),
-                    comment = ""
-                )
-
-            )
-
-            CounterReadingSyncApiClient(AppSettings.backendHost, AppSettings.backendPort).use {
+            val items = unsynchronized.map { it.toSyncItem() }
+            val idMappings = apiClient().use {
                 it.sync(items, testMode = true)
             }
+            if (unsynchronized.size != idMappings.size) {
+                throw Exception("unsynchronized.size != idMappings.size: ${unsynchronized.size} != ${idMappings.size}")
+            }
+            val now = LocalDateTime.now()
+            for (r in unsynchronized) {
+                val serverId =
+                    idMappings.firstOrNull { it.id == r.id }?.serverId
+                        ?: throw Exception("ServerId не найден для Показаний #${r.id}")
+                r.synchronized = true
+                r.syncTime = now
+                r.serverId = serverId
+            }
+            TODO("Save sync results")
         }
+    }
+
+
+    companion object {
+        fun apiClient() =
+            CounterReadingSyncApiClient(AppSettings.backendHost, AppSettings.backendPort)
+
+        fun Iterable<Consumer>.unsynchronizedReadings() =
+            flatMap { it.counters }.flatMap { it.readings }
+                .filter { !it.synchronized }
+
+        fun CounterReading.toSyncItem() = CounterReadingSyncItem(
+            id = id,
+            user = AppSettings.user,
+            counterId = counterId,
+            reading = reading,
+            readingTime = readingTime.toEpochMilli(),
+            comment = comment
+        )
     }
 
     private suspend fun syncInTestMode(allConsumers: List<Consumer>) {
